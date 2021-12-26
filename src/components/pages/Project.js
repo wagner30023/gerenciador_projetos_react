4228ee5cc5dec4b72fa7116068ab307cfb9eb531
Carlos Wagner <wagner30023@gmail.com>
2021-12-26 14:31:47 -0300

    Atualizaçao e detalhes do projeto

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,13 +1,20 @@
 import styles from './Project.module.css'
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+import Message from '../layout/Message'
+import ProjectForm from '../project/ProjectForm'
 
 function Project(){
     const {id} = useParams()
     const [project,setProject] = useState([])
-
+    const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType]  = useState()
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,11 +25,79 @@ function Project(){
               setProject(data)
           })
         .catch((err) => err.json())
+        },1000)
     },[id])
 
+    function editPost(project){
+        // budget validation 
+        if(project.budget < project.cost){
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(data);
+            setShowProjectForm(false)
+            // mensagem
+            setMessage('Projeto atualizado.')
+            setType('success')
+        })
+        .catch((err) => console.log(err))
+    }
+
+    function toggleProjectForm(){
+        setShowProjectForm(!showProjectForm)
+    }
+
     return(
-        <p>{project.name}</p>
+        <>
+            {project.name ? (
+                <div className={styles.project_details}>
+                    <Container customClass="column"> 
+                    {message && <Message type={type} msg={message}/>}
+                    <div className={styles.details_container}>
+                        <h1> Projeto: {project.name}</h1>
+                         <button className={styles.btn} onClick={toggleProjectForm}>
+                              {!showProjectForm ? 'Editar projeto' : 'Fechar'} 
+                         </button>
+                         {!showProjectForm ? (
+                             <div className={styles.project_info}>
+                                 <p> 
+                                    <span> Categoria: </span> {project.category.name}
+                                 </p>
+                                 <p> 
+                                    <span> Total de orçamento: </span> {project.budget}
+                                 </p>
+                                 <p> 
+                                    <span>Total utilizado:</span> R${project.cost}
+                                 </p>
+                             </div>
+                         ) : (
+                             <div className={styles.project_info}>
+                                    <ProjectForm 
+                                        handleSubmit={editPost}
+                                        btnText="Concluir edição"
+                                        projectData={project} /> 
+                             </div>
+                         )}
+                    </div>
+                    </Container>
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </>
     )
+   
 }
 
 export default Project;
