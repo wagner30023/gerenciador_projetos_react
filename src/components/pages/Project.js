3b40ef979c791e536d5146144d12cc86e382700d
Carlos Wagner <wagner30023@gmail.com>
2021-12-27 13:15:14 -0300

    Adicionando novos serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,3 +1,5 @@
+import {  v4 as uuidv4} from 'uuid'
+
 import styles from './Project.module.css'
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
@@ -5,13 +7,17 @@ import Loading from '../layout/Loading'
 import Container from '../layout/Container'
 import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
+import ServiceForm from '../service/ServiceForm'
+
+
 
 function Project(){
     const {id} = useParams()
     const [project,setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
-    const [message, setMessage] = useState()
-    const [type, setType]  = useState()
+    const [showServiceForm, setShowServiceForm] = useState(false)
+    const [message, setMessage] = useState([])
+    const [type, setType]  = useState('success')
     useEffect(() => {
         setTimeout(() => {
             fetch(`http://localhost:5000/projects/${id}`, {
@@ -29,9 +35,10 @@ function Project(){
     },[id])
 
     function editPost(project){
-        // budget validation 
-        if(project.budget < project.cost){
-            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+        setMessage('')
+         // budget validation
+        if (project.budget <= 0) {
+            setMessage('O Orçamento não pode ser menor que o custo do projeto!')
             setType('error')
             return false
         }
@@ -54,8 +61,53 @@ function Project(){
         .catch((err) => console.log(err))
     }
 
+    function createService(project){
+        setMessage('')
+        // last service
+        const lastService = project.services[project.services.length - 1]
+        
+        lastService.id = uuidv4()
+
+        const lastServiceCost = lastService.cost
+
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+        // maximum value validation
+        if(newCost > parseFloat(project.budget)){
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+            setType('error')
+            project.services.pop()
+            return false
+        }
+
+        // add service cost to project total cost
+        project.cost = newCost
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(project),
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            // setServices(data.services)
+            // setShowServiceForm(!showServiceForm)
+            // setMessage('Serviço adicionado!')
+            // setType('success')
+          })
+          .catch((err) => console.log(err))
+
+
+    }
+
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
     }
 
     return(
@@ -90,6 +142,29 @@ function Project(){
                              </div>
                          )}
                     </div>
+                    <div className={styles.service_form_container}>
+                            <h2> Adicione um serviço: </h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                              {!showServiceForm ? 'Adicionar serviço' : 'Fechar'} 
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && (
+                                   <div className={styles.project_info}> 
+                                        {showServiceForm && (
+                                            <ServiceForm 
+                                                handleSubmit={createService}
+                                                btnText="Adicionar serviço"
+                                                projectData={project}
+                                            />
+                                        )}
+                                   </div> 
+                                )}
+                            </div>
+                    </div>
+                    <h2> Serviços </h2>
+                            <Container customClass="start">
+                                <p> Itens de serviços </p>
+                            </Container>
                     </Container>
                 </div>
             ) : (
